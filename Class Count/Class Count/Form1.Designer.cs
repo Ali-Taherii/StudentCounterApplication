b0c40f9fbe97e3bd6a762935b039948d93d94113
Ali Taheri <111309866+Ali-Taherii@users.noreply.github.com>
2023-09-26 14:01:41 +0300

    Project Initiated

---
+++ Class Count/Class Count/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Class_Count
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentList = new System.Windows.Forms.ListBox();
+            this.lblStudents = new System.Windows.Forms.Label();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.btnRemoveStudent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // studentList
+            // 
+            this.studentList.FormattingEnabled = true;
+            this.studentList.ItemHeight = 24;
+            this.studentList.Location = new System.Drawing.Point(47, 78);
+            this.studentList.Name = "studentList";
+            this.studentList.Size = new System.Drawing.Size(236, 388);
+            this.studentList.TabIndex = 0;
+            // 
+            // lblStudents
+            // 
+            this.lblStudents.AutoSize = true;
+            this.lblStudents.Location = new System.Drawing.Point(42, 46);
+            this.lblStudents.Name = "lblStudents";
+            this.lblStudents.Size = new System.Drawing.Size(90, 25);
+            this.lblStudents.TabIndex = 1;
+            this.lblStudents.Text = "Students";
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(47, 472);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(115, 41);
+            this.btnAddStudent.TabIndex = 2;
+            this.btnAddStudent.Text = "Add";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnRemoveStudent
+            // 
+            this.btnRemoveStudent.Enabled = false;
+            this.btnRemoveStudent.Location = new System.Drawing.Point(168, 472);
+            this.btnRemoveStudent.Name = "btnRemoveStudent";
+            this.btnRemoveStudent.Size = new System.Drawing.Size(115, 41);
+            this.btnRemoveStudent.TabIndex = 3;
+            this.btnRemoveStudent.Text = "Remove";
+            this.btnRemoveStudent.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 596);
+            this.Controls.Add(this.btnRemoveStudent);
+            this.Controls.Add(this.btnAddStudent);
+            this.Controls.Add(this.lblStudents);
+            this.Controls.Add(this.studentList);
+            this.Name = "Form1";
+            this.Text = "Students\' class count";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox studentList;
+        private System.Windows.Forms.Label lblStudents;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Button btnRemoveStudent;
+    }
+}
+
---
+++ Class Count/Class Count/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Class Count")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Class Count")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("adb5f4e6-b3a4-445d-95dd-69ae917a9df2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
