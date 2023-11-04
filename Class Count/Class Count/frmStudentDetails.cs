44630842221e7530c31496f1542a0054b4efb409
Ali Taheri <alitaheri473@gmail.com>
2023-11-04 18:46:00 +0200

    Register button adds a student to the db

--- Class Count/Class Count/frmStudentDetails.cs
+++ Class Count/Class Count/frmStudentDetails.cs
@@ -19,8 +19,9 @@ namespace Class_Count
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-            if (txtFirstname.Text.Length != 0 && txtLastname.Text.Length != 0 && numSessions.Value > 0) 
+            if (txtFirstname.Text.Length != 0 && txtLastname.Text.Length != 0 && numSessions.Value > 0)
             {
+                // Create a Student object with the entered data
                 Student student = new Student()
                 {
                     FirstName = txtFirstname.Text,
@@ -28,7 +29,27 @@ namespace Class_Count
                     Payment = dateTimePicker1.Value,
                     Sessions = Convert.ToInt32(numSessions.Value)
                 };
+
+                // Create an instance of the StudentDatabase
+                StudentDatabase studentDB = new StudentDatabase();
+                // Insert the new student into the database
+                studentDB.InsertStudent(student);
+                studentDB.Dispose();
+                
+
+                // Optionally, you can clear the input fields or display a confirmation message.
+                txtFirstname.Clear();
+                txtLastname.Clear();
+                numSessions.Value = 0;
+                dateTimePicker1.Value = DateTime.Now;
+
+                MessageBox.Show("Student registered successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all the required fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
     }
 }
